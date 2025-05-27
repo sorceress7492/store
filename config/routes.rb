9989d90c8fecd91323fbed6c0f6f4116682d95cc
Riya Namdev <riyanamdev.dev@gmail.com>
2025-05-27 19:27:32 +0530

    Added signup up and login

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  resources :users, only: [ :new, :create ], path_names: { new: "signup" }
+  resource :session
+  resources :passwords, param: :token
   get "up" => "rails/health#show", as: :rails_health_check
   resources :products
   root "products#index"
